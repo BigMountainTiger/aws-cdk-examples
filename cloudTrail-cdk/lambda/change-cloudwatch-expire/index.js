0de554588484a5d6b4e96431da3f8a0e841f481d
Song Li <da_tou_li@yahoo.com>
2020-04-30 17:35:49 -0400

    Completing the lambda

--- cloudTrail-cdk/lambda/change-cloudwatch-expire/index.js
+++ cloudTrail-cdk/lambda/change-cloudwatch-expire/index.js
@@ -2,13 +2,13 @@ const AWS = require('aws-sdk');
 
 exports.handler = async (event, context) => {
   console.log(event);
-
+  const logGroupName = ((event.detail || {}).requestParameters || {}).logGroupName;
+  if (! logGroupName) { return { statusCode: 404 }; }
+ 
   await new Promise((rs, rj) => {
-  
-    (new AWS.CloudWatchLogs()).putRetentionPolicy({
-      logGroupName: '/aws/lambda/ALayeredFunction',
-      retentionInDays: 1
-    }, (e, d) => { if (e) { rj(e); } else { rs(d); } });
+
+    (new AWS.CloudWatchLogs()).putRetentionPolicy({ logGroupName: logGroupName, retentionInDays: 1 },
+      (e, d) => { if (e) { rj(e); } else { rs(d); } });
   });
   
   return { statusCode: 200 };
