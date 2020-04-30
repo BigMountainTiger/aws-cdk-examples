0415342b4d3b4d491ccd96f0331249c32a8dd9dd
Song Li <da_tou_li@yahoo.com>
2020-04-30 15:53:54 -0400

    Moved the file locations

--- cloudTrail-cdk/cdk.js
+++ cloudTrail-cdk/cdk.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const cdk = require('@aws-cdk/core');
-const { CloudTrailCdkStack } = require('./lib/cloud_trail-cdk-stack');
+const { CloudTrailCdkStack } = require('./bin/cloud_trail-cdk-stack');
 
 const app = new cdk.App();
 new CloudTrailCdkStack(app, 'CloudTrailCdkStack');
