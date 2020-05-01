485e8fbb5edc2ff01c6920b748d5b5f2961c859d
Song Li <da_tou_li@yahoo.com>
2020-05-01 11:07:47 -0400

    Tested the dynamo client

--- dynamo-cdk/dynamo-test-client.js
+++ dynamo-cdk/dynamo-test-client.js
@@ -4,12 +4,30 @@ AWS.config.update({
   region: "us-east-1",
 });
 
-var docClient = new AWS.DynamoDB.DocumentClient();
+const docClient = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = 'DynamoCdkStackDynamoTable';
 
-docClient.scan({TableName: 'DynamoCdkStackDynamoTable'}, (e, d) => {
-  if (e) {
-    console.log(e);
-  } else {
-    console.log(d);
-  }
-})
+(async () => {
+
+  const item = {
+    TableName: TABLE_NAME,
+    Item: {id: 'A', name: 'This is A'}
+  };
+
+  await new Promise((rs, rj) => {
+    docClient.put(item, (e, d) => {
+      if (e) { rj(e); } else { rs(d); }
+    })
+  });
+
+  docClient.scan({TableName: TABLE_NAME}, (e, d) => {
+    if (e) {
+      console.log(e);
+    } else {
+      console.log(d);
+    }
+  })
+
+})();
+
+console.log('Initiated ...');
