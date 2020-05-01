53d5e5f9641ca9731362eff5334a8e0f624ab23d
Song Li <da_tou_li@yahoo.com>
2020-05-01 09:55:08 -0400

    Added the helper note

--- dynamo-cdk/lib/dynamo-cdk-stack.js
+++ dynamo-cdk/lib/dynamo-cdk-stack.js
@@ -1,16 +1,10 @@
 const cdk = require('@aws-cdk/core');
 
 class DynamoCdkStack extends cdk.Stack {
-  /**
-   *
-   * @param {cdk.Construct} scope
-   * @param {string} id
-   * @param {cdk.StackProps=} props
-   */
+
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
   }
 }
 
