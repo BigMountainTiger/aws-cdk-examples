d86e9e0f631bcd008c61f50c687e768d1a284bb5
Song Li <da_tou_li@yahoo.com>
2020-05-01 10:11:42 -0400

    Added the dynamo resource to the stack

--- dynamo-cdk/lib/dynamo-cdk-stack.js
+++ dynamo-cdk/lib/dynamo-cdk-stack.js
@@ -1,10 +1,25 @@
 const cdk = require('@aws-cdk/core');
+const dynamodb = require('@aws-cdk/aws-dynamodb');
 
 class DynamoCdkStack extends cdk.Stack {
 
   constructor(scope, id, props) {
     super(scope, id, props);
 
+    const PREFIX = id;
+    const DYNAMO_TABLE = `${PREFIX}DynamoTable`
+
+    const add_table = () => {
+      const table = new dynamodb.Table(this, DYNAMO_TABLE, {
+        billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+        tableName: DYNAMO_TABLE,
+        partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING }
+      });
+
+      return table;
+    };
+    
+    add_table();
   }
 }
 
