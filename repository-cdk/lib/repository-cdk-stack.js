a0db549ecc58e369f270c167cbfc6af209a2eaf1
Song Li <da_tou_li@yahoo.com>
2020-05-01 11:47:46 -0400

    Completing the cdk repository test

--- repository-cdk/lib/repository-cdk-stack.js
+++ repository-cdk/lib/repository-cdk-stack.js
@@ -1,9 +1,24 @@
 const cdk = require('@aws-cdk/core');
+const codecommit = require('@aws-cdk/aws-codecommit');
 
 class RepositoryCdkStack extends cdk.Stack {
 
   constructor(scope, id, props) {
     super(scope, id, props);
+
+    const ID = id;
+    const REPOSITORY_NAME = `${ID}Repository`;
+
+    const add_repository = () => {
+      const repo = new codecommit.Repository(this, REPOSITORY_NAME ,{
+        repositoryName: REPOSITORY_NAME,
+        description: 'This is a test repository.'
+      });
+
+      return repo;
+    };
+
+    add_repository();
 
   }
 }
