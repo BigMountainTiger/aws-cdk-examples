9f5f1a49ea038aed9a18f13341c05d9863f73706
Song Li <da_tou_li@yahoo.com>
2020-05-03 17:03:06 -0400

    Added the user pool

--- cognito-cdk/lib/cognito-cdk-stack.js
+++ cognito-cdk/lib/cognito-cdk-stack.js
@@ -1,16 +1,29 @@
 const cdk = require('@aws-cdk/core');
+const cognito = require('@aws-cdk/aws-cognito');
 
 class CognitoCdkStack extends cdk.Stack {
-  /**
-   *
-   * @param {cdk.Construct} scope
-   * @param {string} id
-   * @param {cdk.StackProps=} props
-   */
+
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const ID = id;
+    const USERPOOL_NAME = `${ID}UserPool`;
+
+    const add_user_pool = () => {
+      const pool = new cognito.UserPool(this, USERPOOL_NAME, {
+        userPoolName: USERPOOL_NAME,
+        passwordPolicy: {},
+        userInvitation: {
+          emailSubject: 'Invite to join our awesome app!',
+          emailBody: 'Hello {username}, you have been invited to join our awesome app! Your temporary password is {####}',
+          smsMessage: 'Hello {username}, your temporary password for our awesome app is {####}'
+        }
+      });
+
+      return pool;
+    };
+
+    add_user_pool();
   }
 }
 
