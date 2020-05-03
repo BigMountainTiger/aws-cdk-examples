d7cfe56f64d1a92873d9fb6fd872a36f6c4c25dd
Song Li <da_tou_li@yahoo.com>
2020-05-03 19:51:27 -0400

    Added the identity pool

--- cognito-cdk/lib/cognito-cdk-stack.js
+++ cognito-cdk/lib/cognito-cdk-stack.js
@@ -9,9 +9,10 @@ class CognitoCdkStack extends cdk.Stack {
     const ID = id;
     const USERPOOL_NAME = `${ID}UserPool`;
     const USERPOOL_CLIENT_NAME = `${USERPOOL_NAME}Client`;
+    const IDENTITY_POOL_NAME = `${ID}IdentityPool`;
 
     const add_user_pool = () => {
-      const pool = new cognito.UserPool(this, USERPOOL_NAME, {
+      const user_pool = new cognito.UserPool(this, USERPOOL_NAME, {
         userPoolName: USERPOOL_NAME,
         passwordPolicy: {},
         userInvitation: {
@@ -23,11 +24,11 @@ class CognitoCdkStack extends cdk.Stack {
         }
       });
 
-      return pool;
+      return user_pool;
     };
 
     const add_user_pool_client = (pool) => {
-      const client = new cognito.UserPoolClient(this, USERPOOL_CLIENT_NAME, {
+      const user_pool_client = new cognito.UserPoolClient(this, USERPOOL_CLIENT_NAME, {
         userPool: pool,
         userPoolClientName: USERPOOL_CLIENT_NAME,
         generateSecret: false,
@@ -38,12 +39,25 @@ class CognitoCdkStack extends cdk.Stack {
         }
       })
 
-      return client;
+      return user_pool_client;
     };
 
-    const pool = add_user_pool();
-    const client = add_user_pool_client(pool);
+    const add_identity_pool = (user_pool, user_pool_client) => {
+      const identity_pool = new cognito.CfnIdentityPool(this, IDENTITY_POOL_NAME, {
+        identityPoolName: IDENTITY_POOL_NAME,
+        allowUnauthenticatedIdentities: false,
+        cognitoIdentityProviders: [{
+            clientId: user_pool_client.userPoolClientId,
+            providerName: user_pool.userPoolProviderName,
+          }]
+      });
 
+      return identity_pool;
+    }
+
+    const user_pool = add_user_pool();
+    const user_pool_client = add_user_pool_client(user_pool);
+    add_identity_pool(user_pool, user_pool_client);
   }
 }
 
