e00132317e29f5f9253c8789667556403f1b39fe
Song Li <da_tou_li@yahoo.com>
2020-05-07 13:41:34 -0400

    Added the client axios call using the aws keys

--- cognito-cdk/client/login.js
+++ cognito-cdk/client/login.js
@@ -1,5 +1,7 @@
 const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 const AWS = require('aws-sdk');
+const axios = require('axios');
+const aws4 = require('aws4');
 global.fetch = require('node-fetch');
 
 const REGION = 'us-east-1';
@@ -7,6 +9,7 @@ const USER_POOL_ID = 'us-east-1_qzRHIZK9W';
 const USER_POOL_CLIENT_ID = '76fduugu2k2pk86ho3l94mf2ju';
 const IDENTITY_POOL_ID = 'us-east-1:79400f5a-1321-45d9-8345-84bf45996d20';
 const IDENTITY_PROVIDER_NAME = 'cognito-idp.us-east-1.amazonaws.com/us-east-1_qzRHIZK9W'
+const API_URL = ' https://i41ty53jd2.execute-api.us-east-1.amazonaws.com/prod';
 
 const USER = 'song';
 const PASSWORD = 'Password123';
@@ -73,12 +76,38 @@ const get_keys = async (token) => {
   return await p;  
 };
 
+const make_api_call = async (identities) => {
+
+  let option = {
+    hostname: 'i41ty53jd2.execute-api.us-east-1.amazonaws.com',
+    method: "GET",
+    url: 'https://i41ty53jd2.execute-api.us-east-1.amazonaws.com/prod',
+    path: '/prod'
+  };
+
+  option = aws4.sign(option,
+    {
+      accessKeyId: identities.accessKeyId,
+      secretAccessKey: identities.secretAccessKey,
+      sessionToken: identities.sessionToken
+    });
+
+  delete option.headers['Host']
+  delete option.headers['Content-Length']
+
+  return await axios(option);
+};
 
 (async () => {
   const token = await login();
   const identities = await get_keys(token);
 
-  console.log(identities);
+  try {
+    const result = await make_api_call(identities);
+    console.log(result);
+  } catch(e) {
+    console.log(e);
+  }
 
 })();
 
