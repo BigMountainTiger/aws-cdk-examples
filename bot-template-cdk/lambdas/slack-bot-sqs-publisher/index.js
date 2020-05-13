be0e4071d2e1bd074502481ef0a654c582748eb9
Song Li <da_tou_li@yahoo.com>
2020-05-13 18:02:51 -0400

    Added the publish lambda implementation

---
+++ bot-template-cdk/lambdas/slack-bot-sqs-publisher/index.js
@@ -0,0 +1,37 @@
+const SUPPORT_QUEUE_REGION = 'us-east-1';
+const QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/005256505030/TEST_TECH_SUPPORT_QUEUE';
+
+const AWS = require('aws-sdk');
+AWS.config.update({region: SUPPORT_QUEUE_REGION});
+
+exports.handler = async (event, context) => {
+  
+  let sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+  
+  let params = {
+    DelaySeconds: 0,
+    MessageAttributes: { "Type": { DataType: "String", StringValue: "TEST" }},
+    MessageBody: JSON.stringify('This is from the sqs message producer'),
+    QueueUrl: QUEUE_URL
+  };
+
+  let request = new Promise((resolve, reject) => {
+    sqs.sendMessage(params, (err, data) => {
+      if (err) { reject(err); } else { resolve(data.MessageId) }
+    });
+  });
+
+  let response = null;
+  try {
+    response = await request;
+  } catch(e) {
+    response = e;
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: response
+    })
+  };
+};
