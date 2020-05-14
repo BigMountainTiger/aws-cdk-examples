3b1bc2521997d6e7f27889e356d9c51f2c95adb7
Song Li <da_tou_li@yahoo.com>
2020-05-14 14:29:12 -0400

    Test using the layer

--- bot-template-cdk/lambdas/slack-bot-sqs-consumer/index.js
+++ bot-template-cdk/lambdas/slack-bot-sqs-consumer/index.js
@@ -1,10 +1,14 @@
+const tuc = require('temp-units-conv');
+
 exports.handler = async (event, context) => {
   console.log(event);
   
+  const c = tuc['f2c'](86);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'This is the lambda'
+      message: `This is the lambda - ${c}`
     })
   };
 };
