38718862ca3ffaecbe28b48c9ed4db91735266d1
Song Li <da_tou_li@yahoo.com>
2020-05-14 17:07:09 -0400

    Removed the dependency on temp-units-conv

--- bot-template-cdk/lambdas/slack-bot-sqs-consumer/index.js
+++ bot-template-cdk/lambdas/slack-bot-sqs-consumer/index.js
@@ -1,14 +1,11 @@
-const tuc = require('temp-units-conv');
-
 exports.handler = async (event, context) => {
+
   console.log(event);
-  
-  const c = tuc['f2c'](86);
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `This is the lambda - ${c}`
+      message: `This is the lambda`
     })
   };
 };
--- bot-template-cdk/lambdas/slack-bot-sqs-publisher/index.js
+++ bot-template-cdk/lambdas/slack-bot-sqs-publisher/index.js
@@ -1,19 +1,16 @@
 const SUPPORT_QUEUE_REGION = 'us-east-1';
 const QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/005256505030/SLACK_TECH_SUPPORT_QUEUE';
 
-const tuc = require('temp-units-conv');
-
 const AWS = require('aws-sdk');
 AWS.config.update({region: SUPPORT_QUEUE_REGION});
 
 exports.handler = async (event, context) => {
-  const c = tuc['f2c'](86);
   let sqs = new AWS.SQS({apiVersion: '2012-11-05'});
   
   let params = {
     DelaySeconds: 0,
     MessageAttributes: { "Type": { DataType: "String", StringValue: "TEST" }},
-    MessageBody: JSON.stringify(`This is from the sqs message producer - ${c}`),
+    MessageBody: JSON.stringify(`This is from the sqs message producer`),
     QueueUrl: QUEUE_URL
   };
 
