e9fddc24d2b9302424a47f04196632099a849f42
Song Li <da_tou_li@yahoo.com>
2020-05-15 09:57:03 -0400

    Added description to the stack

--- slack-bot-jira-app/cdk.js
+++ slack-bot-jira-app/cdk.js
@@ -4,4 +4,7 @@ const cdk = require('@aws-cdk/core');
 const { BotTemplateCdkStack } = require('./lib/slack-bot-jira-app-stack');
 
 const app = new cdk.App();
-new BotTemplateCdkStack(app, 'SLACK-BOT-JIRA-APP-STACK');
+const STACK_NAME = 'SLACK-BOT-JIRA-APP-STACK';
+new BotTemplateCdkStack(app, STACK_NAME, {
+  description: STACK_NAME
+});
