693e04adef5cfb51a706530eb50790e2ff085fca
Song Li <da_tou_li@yahoo.com>
2020-05-26 15:25:28 -0400

    Replaced the standard queue with a FIFO queue

--- slack-bot-jira-app/A-playground/sqs/send-message.js
+++ slack-bot-jira-app/A-playground/sqs/send-message.js
@@ -1,6 +1,6 @@
 const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
-AWS.config.update({region: process.env.SUPPORT_QUEUE_REGION});
+AWS.config.update({region: 'us-east-1'});
 
 const sendData = async (data) => {
   let sqs = new AWS.SQS({apiVersion: '2012-11-05'});
@@ -13,7 +13,7 @@ const sendData = async (data) => {
     MessageDeduplicationId: uuidv4(),
     MessageGroupId: 'TECH_SUPPORT_REQUEST_GROUP_ID',
     MessageBody: JSON.stringify(data),
-    QueueUrl: process.env.SUPPORT_QUEUE_URL
+    QueueUrl: `https://sqs.us-east-1.amazonaws.com/005256505030/SLACK_TECH_SUPPORT_QUEUE.fifo`
   };
 
   let request = new Promise((resolve, reject) => {
@@ -25,4 +25,8 @@ const sendData = async (data) => {
   await request;
 };
 
-exports.sendData = sendData;
+(async () => {
+  await sendData('Test data');
+})();
+
+console.log('Initiated ...');
--- slack-bot-jira-app/lib/resources/add_sqs.js
+++ slack-bot-jira-app/lib/resources/add_sqs.js
@@ -7,7 +7,8 @@ const add_sqs = (scope) => {
 
   const name = NAME.SQS_NAME;
   const queue = new sqs.Queue(scope, name, {
-    queueName: name,
+    queueName: `${name}.fifo`,
+    fifo: true,
     receiveMessageWaitTime: cdk.Duration.seconds(20),
     visibilityTimeout: cdk.Duration.minutes(5)
   });
