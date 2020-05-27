245db06b57bec15671d085fa775962b4c1260e63
Song Li <da_tou_li@yahoo.com>
2020-05-27 17:10:02 -0400

    Added test events

--- snowflak-pipe-automation/lambdas/snowflake-s3-pipe-starter/app.js
+++ snowflak-pipe-automation/lambdas/snowflake-s3-pipe-starter/app.js
@@ -1,7 +1,7 @@
 const snowflake_command = require('snowflake-command');
 
 exports.lambdaHandler = async (event, context) => {
-  console.log(event);
+  console.log(JSON.stringify(event));
   const query = `ALTER TASK IF EXISTS Load_Data_Task RESUME;`;
   await snowflake_command.issue(query);
 
