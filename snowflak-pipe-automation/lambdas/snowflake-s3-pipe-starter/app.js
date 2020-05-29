51ed8f4dfb0cf17d8c1ea9019771fd2e260635f0
Song Li <da_tou_li@yahoo.com>
2020-05-29 10:59:31 -0400

    Working on the file to task mapping

--- snowflak-pipe-automation/lambdas/snowflake-s3-pipe-starter/app.js
+++ snowflak-pipe-automation/lambdas/snowflake-s3-pipe-starter/app.js
@@ -1,9 +1,46 @@
 const snowflake_command = require('snowflake-command');
 
+const get_file_list = (event) => {
+
+  const files = [];
+  const records = event.Records || [];
+
+  records.forEach((record) => {
+    const body = JSON.parse(record.body || '{}');
+    const s3_records = body.Records || [];
+
+    s3_records.forEach((s3_record) => {
+      const s3 = s3_record.s3 || {};
+      const s3_object = s3.object || {};
+      const s3_object_key = s3_object.key;
+
+      if (s3_object_key) {
+        files.push(s3_object_key);
+      }
+    });
+    
+  })
+
+  return files;
+};
+
+const get_query_list = (files) => {
+
+  const queries = [];
+  files.forEach((file) => {
+
+  });
+
+  return queries;
+};
+
 exports.lambdaHandler = async (event, context) => {
-  console.log(JSON.stringify(event));
-  const query = `ALTER TASK IF EXISTS Load_Data_Task RESUME;`;
-  await snowflake_command.issue(query);
+
+  const files = get_file_list(event);
+  console.log(files);
+
+  // const query = `ALTER TASK IF EXISTS Load_Data_Task RESUME;`;
+  // await snowflake_command.issue(query);
 
   return {};
 };
