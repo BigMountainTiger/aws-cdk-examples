53a738036387e96bbbfae4d2fedd206ab5d9abb0
Song Li <da_tou_li@yahoo.com>
2020-06-08 17:38:14 -0400

    Keep the code

--- s3-event-cdk/lambdas/s3-handler/app.js
+++ s3-event-cdk/lambdas/s3-handler/app.js
@@ -1,5 +1,6 @@
 exports.lambdaHandler = async (event, context) => {
 
   console.log(event);
+  console.log(JSON.stringify(event));
   return {};
 };
--- s3-event-cdk/lib/s3-event-cdk-stack.js
+++ s3-event-cdk/lib/s3-event-cdk-stack.js
@@ -1,4 +1,8 @@
 const cdk = require('@aws-cdk/core');
+
+const s3 = require('@aws-cdk/aws-s3');
+const s3_event_source = require('@aws-cdk/aws-lambda-event-sources');
+
 const s3_bucket = require('./resources/s3-bucket');
 const s3_event_lambda_handler = require("./resources/s3-event-lambda-handler");
 
@@ -9,6 +13,9 @@ class S3EventCdkStack extends cdk.Stack {
     const bucket = s3_bucket(this);
     const handler = s3_event_lambda_handler(this);
     
+    handler.addEventSource(new s3_event_source.S3EventSource(bucket, {
+      events: [ s3.EventType.OBJECT_CREATED ]
+    }));
   }
 }
 
