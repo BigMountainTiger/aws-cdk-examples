7b282c292b38e9bf2fc1aedcb127a5df3ac5b1d4
Song Li <da_tou_li@yahoo.com>
2020-06-08 16:56:39 -0400

    Initial test of the lambda

---
+++ s3-event-cdk/lib/resources/s3-event-lambda-handler.js
@@ -0,0 +1,33 @@
+const cdk = require('@aws-cdk/core');
+const iam = require('@aws-cdk/aws-iam');
+const lambda = require('@aws-cdk/aws-lambda');
+
+const add_lambda = (scope) => {
+
+  const role_name = 'S3_EVENT_LAMBDA_HANDLER_ROLE';
+  const role = new iam.Role(scope, role_name, {
+    roleName: role_name,
+    description: role_name,
+    assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+  });
+
+  role.addToPolicy(new iam.PolicyStatement({
+    effect: iam.Effect.ALLOW,
+    resources: ['*'],
+    actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents']
+  }))
+
+  const lambda_name = 'S3_EVENT_LAMBDA_HANDLER';
+  return new lambda.Function(scope, lambda_name, {
+    runtime: lambda.Runtime.NODEJS_12_X,
+    functionName: lambda_name,
+    description: lambda_name,
+    timeout: cdk.Duration.seconds(15),
+    role: role,
+    code: lambda.Code.asset('./lambdas/s3-handler'),
+    memorySize: 256,
+    handler: 'app.lambdaHandler'
+  });
+};
+
+module.exports = add_lambda;
--- s3-event-cdk/lib/s3-event-cdk-stack.js
+++ s3-event-cdk/lib/s3-event-cdk-stack.js
@@ -1,11 +1,13 @@
 const cdk = require('@aws-cdk/core');
 const s3_bucket = require('./resources/s3-bucket');
+const s3_event_lambda_handler = require("./resources/s3-event-lambda-handler");
 
 class S3EventCdkStack extends cdk.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
    
     const bucket = s3_bucket(this);
+    const handler = s3_event_lambda_handler(this);
     
   }
 }
