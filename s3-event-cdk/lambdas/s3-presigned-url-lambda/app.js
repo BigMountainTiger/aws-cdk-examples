0f399c528a34ce06efbbeb7371142107cf2d8e3b
Song Li <da_tou_li@yahoo.com>
2020-06-09 14:01:51 -0400

    Keep the code

--- s3-event-cdk/lambdas/s3-presigned-url-lambda/app.js
+++ s3-event-cdk/lambdas/s3-presigned-url-lambda/app.js
@@ -1,3 +1,6 @@
+// https://medium.com/@aakashbanerjee/upload-files-to-amazon-s3-from-the-browser-using-pre-signed-urls-4602a9a90eb5
+// Get presigned url.
+
 exports.lambdaHandler = async (event, context) => {
 
   console.log(event);
--- s3-event-cdk/lib/s3-event-cdk-stack.js
+++ s3-event-cdk/lib/s3-event-cdk-stack.js
@@ -25,7 +25,4 @@ class S3EventCdkStack extends cdk.Stack {
   }
 }
 
-// https://medium.com/@aakashbanerjee/upload-files-to-amazon-s3-from-the-browser-using-pre-signed-urls-4602a9a90eb5
-// Get presigned url.
-
 module.exports = { S3EventCdkStack }
