64680b72fc881461c60ffb24d6044c06f9e39ec4
Song Li <da_tou_li@yahoo.com>
2020-06-10 08:49:44 -0400

    Added STEP-FUNCTION-CDK-STACK

---
+++ step-function-cdk/cdk.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const cdk = require('@aws-cdk/core');
+const { StepFunctionCdkStack } = require('./lib/step-function-cdk-stack');
+
+const app = new cdk.App();
+const STACK_NAME = 'STEP-FUNCTION-CDK-STACK';
+new StepFunctionCdkStack(app, STACK_NAME, {
+  description: STACK_NAME
+});
