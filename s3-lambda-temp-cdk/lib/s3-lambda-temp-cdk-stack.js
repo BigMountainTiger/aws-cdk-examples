609d45ce55b01b333e786ab7614704e81182fc74
Song Li <da_tou_li@yahoo.com>
2020-06-15 13:34:25 -0400

    Added the S3 Bucket to the stacke

--- s3-lambda-temp-cdk/lib/s3-lambda-temp-cdk-stack.js
+++ s3-lambda-temp-cdk/lib/s3-lambda-temp-cdk-stack.js
@@ -1,9 +1,18 @@
 const cdk = require('@aws-cdk/core');
+const s3 = require('@aws-cdk/aws-s3');
 
 class S3LambdaTempCdkStack extends cdk.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
+    const BUCKET_NAME = 'S3_LAMBDA_TEMP_BUCKET';
+    new s3.Bucket(this, BUCKET_NAME, {
+      bucketName: `${BUCKET_NAME.toLowerCase().replace(/_/g, '-')}.huge.head.li`,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      versioned: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    
     
   }
 }
