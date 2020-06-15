7718d7b7e47df449e7c597e5fad57069d9438b84
Song Li <da_tou_li@yahoo.com>
2020-06-15 16:35:28 -0400

    Completing the example

--- s3-lambda-temp-cdk/lambdas/s3-lambda-temp-lambda/app.js
+++ s3-lambda-temp-cdk/lambdas/s3-lambda-temp-lambda/app.js
@@ -2,11 +2,12 @@
 
 const AWS = require('aws-sdk');
 const fs = require('fs');
+const readline = require('readline');
 
 const bucket = 's3-lambda-temp-bucket.huge.head.li';
 const SOURCE_KEY = 'Loan.txt';
 
-const get_object = async () => {
+const get_s3_object = async () => {
   const s3 = new AWS.S3();
 
   const params = {
@@ -19,34 +20,72 @@ const get_object = async () => {
   return file;
 };
 
-const put_object = async (text) => {
+const put_s3_object = async (buffer) => {
   const s3 = new AWS.S3();
   
   const time = new Date().toISOString();
   const TARGET_KEY = time.replace(/:/g, '-').replace(/\./g, '-');
 
-  const buffer = Buffer.from(text, 'utf8');
   const params = {
     Bucket: bucket,
     Key: TARGET_KEY,
-    Body: buffer,
-    ContentType: "text/html"
+    Body: buffer
   };
 
   const result = await s3.putObject(params).promise(); 
   return result;
 };
 
+const write_to_file = async (file_name, buffer) => {
+  const write_p = new Promise((rs, rj) => {
+    fs.writeFile(file_name, buffer, (err) => {
+      if(err) { rj(err); } else { rs('File is saved'); }
+    });
+  });
+
+  await write_p;
+};
+
+const read_file = async (file_name) => {
+  const read_p = new Promise((rs, rj) => {
+    fs.readFile(file_name, (err, data) => {
+      if(err) { rj(err); } else { rs(data); }
+    });
+  });
+  
+  return await read_p;
+};
+
+const read_file_line_by_line = async (file_name) => {
+  const fileStream = fs.createReadStream(file_name);
+  const rl = readline.createInterface({ input: fileStream, crlfDelay: Infinity });
+
+  const buffers = [];
+  for await (const line of rl) {
+    const sub_line = line.split('|').slice(0, 3).join('|') + '\n';
+    buffers.push(Buffer.from(sub_line));
+  }
+
+  return Buffer.concat(buffers);
+};
+
 exports.lambdaHandler = async (event, context) => {
   let result = {};
 
-  const file = await get_object();
+  const file = await get_s3_object();
   const buffer = file.Body || new Buffer(0);
 
-  console.log(buffer);
+  const file_name = '/tmp/tempfile';
 
-  // const result = await put_object(text);
-  // console.log(result);
+  await write_to_file(file_name, buffer);
+
+  const put_parital_file = false;
+  const func = put_parital_file? read_file_line_by_line : read_file;
+  const data = await func(file_name);
+
+  result = await put_s3_object(data);
+
+  console.log(result);
 
   return {
     statusCode: 200,
