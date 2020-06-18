c57c039c74e68f4c750df59533ac74b0261d29b7
Song Li <da_tou_li@yahoo.com>
2020-06-18 11:04:16 -0400

    Keeping the code

--- docker-cdk/stack/example-task-docker-cdk-stack.js
+++ docker-cdk/stack/example-task-docker-cdk-stack.js
@@ -9,10 +9,14 @@
 // CDK example
 // https://docs.aws.amazon.com/cdk/latest/guide/ecs_example.html
 
+// Additional
+// https://blog.kylegalbraith.com/2019/10/24/how-to-run-docker-containers-via-aws-elastic-container-service/
+
 const cdk = require('@aws-cdk/core');
 const ec2 = require("@aws-cdk/aws-ec2");
 const ecs = require("@aws-cdk/aws-ecs");
-const ecs_patterns = require("@aws-cdk/aws-ecs-patterns");
+const sfn = require('@aws-cdk/aws-stepfunctions');
+const tasks = require('@aws-cdk/aws-stepfunctions-tasks');
 
 class ExampleTaskDockerCdkStack extends cdk.Stack {
 
@@ -25,21 +29,29 @@ class ExampleTaskDockerCdkStack extends cdk.Stack {
     const CLUSTER_NAME = `${id}-CLUSTER`;
     const cluster = new ecs.Cluster(this, CLUSTER_NAME, { vpc: vpc });
 
-    const image = ecs.ContainerImage.fromAsset('./docker/express-app');
-    const CONTAINER_NAME = `${id}-CONTAINER`;
-    const container = new ecs.ContainerDefinition(this. CONTAINER_NAME, {
-      image: image
+    // const image = ecs.ContainerImage.fromAsset('./docker/experiment-1');
+    // const CONTAINER_NAME = `${id}-CONTAINER`;
+    // const container = new ecs.ContainerDefinition(this. CONTAINER_NAME, {
+    // });
+
+    const STEP1_NAME = `${id}-STEP-1`;
+    const asset_Path = './docker/experiment-1';
+    const image = tasks.DockerImage.fromAsset(this, STEP1_NAME, {
+      directory: asset_Path
     });
 
-    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
-      cluster: cluster,
-      cpu: 256,
-      desiredCount: 1,
-      taskImageOptions: { image: image },
-      memoryLimitMiB: 512,
-      publicLoadBalancer: true
-    });
+    // const step_1 = new tasks.RunEcsFargateTask({
+    //   cluster: cluster,
+    // });
+    
+    // const definition = image;
 
+    // const STATE_MACHINE_NAME = `${id}-STEP-TEST-STATE-MACHINE`;
+    // new sfn.StateMachine(this, STATE_MACHINE_NAME, {
+    //   stateMachineName: STATE_MACHINE_NAME,
+    //   definition,
+    //   timeout: cdk.Duration.minutes(5)
+    // });
     
   }
 }
