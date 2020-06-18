62ffd77b2803510bfa4f5b0268a8b35e257d28af
Song Li <da_tou_li@yahoo.com>
2020-06-18 09:56:18 -0400

    Added a new stack to keep the code

--- docker-cdk/cdk.js
+++ docker-cdk/cdk.js
@@ -2,9 +2,16 @@
 
 const cdk = require('@aws-cdk/core');
 const { ExampleWebDockerCdkStack } = require('./stack/example-web-docker-cdk-stack');
+const { ExampleTaskDockerCdkStack } = require('./stack/example-task-docker-cdk-stack');
 
 const app = new cdk.App();
-const NAME = 'EXAMPLE-WEB-DOCKER-CDK-STACK';
-new ExampleWebDockerCdkStack(app, NAME, {
-  description: NAME
+
+const EXAMPLE_WEB_NAME = 'EXAMPLE-WEB-DOCKER-CDK-STACK';
+new ExampleWebDockerCdkStack(app, EXAMPLE_WEB_NAME, {
+  description: EXAMPLE_WEB_NAME
+});
+
+const EXAMPLE_TASK_NAME = 'EXAMPLE-TASK-DOCKER-CDK-STACK';
+new ExampleTaskDockerCdkStack(app, EXAMPLE_TASK_NAME, {
+  description: EXAMPLE_TASK_NAME
 });
