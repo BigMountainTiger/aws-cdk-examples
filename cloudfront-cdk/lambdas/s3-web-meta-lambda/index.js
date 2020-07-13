1aff2e4f0b7cc0bafec8ade61773cadef57f6d3a
Song Li <da_tou_li@yahoo.com>
2020-07-13 18:01:21 -0400

    Add more data to it

--- cloudfront-cdk/lambdas/s3-web-meta-lambda/index.js
+++ cloudfront-cdk/lambdas/s3-web-meta-lambda/index.js
@@ -3,20 +3,55 @@ AWS.config.update({ region: 'us-east-1' });
 
 exports.lambdaHandler = async (event, context) => {
   
-  const s3 = event.Records[0].s3;
+  const s3 = event.Records[0].s3;  
+  var bucket = s3.bucket.name;
+  var key =  decodeURIComponent(s3.object.key.replace(/\+/g, ' '));
 
-  console.log(s3);
+  let params = {
+    Bucket: bucket,
+    Key: key
+  };
+
+  const o = await new Promise((rs, rj) => {
+    const aws_s3 = new AWS.S3();
+
+    aws_s3.getObject(params, (e, d) => {
+      if (e) { rj(e); } else { rs(d); }
+    });
+  });
+
+  const contentType = o.ContentType;
+  const metadata = o.Metadata;
   
-  const aws_s3 = new AWS.S3();
-  
+  params = {
+      Bucket: bucket,
+      Key: key,
+      CopySource: encodeURIComponent(`${bucket}/${key}`),
+      ContentType: contentType,
+      Metadata: Object.assign(metadata, {
+        "Cache-Control": "no-cache, no-store, must-revalidate",
+        "Pragma": "no-cache",
+        "Expires": "-1"
+      }),
+      MetadataDirective: 'COPY'
+  };
+
+  const result = await new Promise((rs, rj) => {
+    const aws_s3 = new AWS.S3();
+
+    aws_s3.copyObject(params, (e, d) => {
+      if (e) { rj(e); } else { rs(d); }
+    });
+
+  });
+
+  console.log(result);
 
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json'
-    }, body: JSON.stringify({
-      message: 'OK'
-    })
+    }, body: JSON.stringify(result)
   };
 
 };
