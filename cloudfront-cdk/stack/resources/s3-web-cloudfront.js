31e20f7af190d8188aef091bce80be9313568482
Song Li <da_tou_li@yahoo.com>
2020-07-13 19:18:20 -0400

    Keep the code

--- cloudfront-cdk/stack/resources/s3-web-cloudfront.js
+++ cloudfront-cdk/stack/resources/s3-web-cloudfront.js
@@ -26,7 +26,7 @@ const add_cloudfront = (scope, id, bucket) => {
           originAccessIdentity: oai
         },
         behaviors: [
-          { isDefaultBehavior: true , default_ttl: 31536000}
+          { isDefaultBehavior: true , default_ttl: 31536000 }
         ],
       }
     ],
