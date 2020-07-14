2020d689fb96f19310e65e3f9cbcaf18dc6f3b27
Song Li <da_tou_li@yahoo.com>
2020-07-14 19:19:09 -0400

    Added the codebuild project

--- codebuild-cdk/stacks/codebuild-cdk-stack.js
+++ codebuild-cdk/stacks/codebuild-cdk-stack.js
@@ -1,10 +1,30 @@
+// https://docs.aws.amazon.com/cdk/api/latest/docs/aws-codebuild-readme.html
+
 const cdk = require('@aws-cdk/core');
+const codebuild = require('@aws-cdk/aws-codebuild');
 
 class CodebuildCdkStack extends cdk.Stack {
 
   constructor(scope, id, props) {
     super(scope, id, props);
 
+    const projectName = `${id}-MY-PROJECT`;
+    new codebuild.Project(this, projectName, {
+      projectName: projectName,
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          build: {
+            commands: [
+              'ls -la',
+              'pwd',
+              'cat /etc/lsb-release'
+            ]
+          }
+        }
+      }),
+      environment: codebuild.LinuxBuildImage.STANDARD_4_0
+    });
     
   }
 }
