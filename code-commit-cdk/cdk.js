85d219f3f77acc3f9515ffd04d72c69dd0461cf4
Song Li <da_tou_li@yahoo.com>
2020-07-17 14:39:45 -0400

    Refactored the code

--- code-commit-cdk/cdk.js
+++ code-commit-cdk/cdk.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const cdk = require('@aws-cdk/core');
-const { CodeCommitCdkStack } = require('../lib/code-commit-cdk-stack');
+const { CodeCommitCdkStack } = require('./stacks/code-commit-cdk-stack');
 
 const app = new cdk.App();
 new CodeCommitCdkStack(app, 'CodeCommitCdkStack');
--- code-commit-cdk/stacks/code-commit-cdk-stack.js
+++ code-commit-cdk/stacks/code-commit-cdk-stack.js
@@ -1,16 +1,11 @@
 const cdk = require('@aws-cdk/core');
 
 class CodeCommitCdkStack extends cdk.Stack {
-  /**
-   *
-   * @param {cdk.Construct} scope
-   * @param {string} id
-   * @param {cdk.StackProps=} props
-   */
+
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    
   }
 }
 
