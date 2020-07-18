00c48d534bc8b71fcc542e57904318f74c1cfa31
Song Li <da_tou_li@yahoo.com>
2020-07-18 17:52:33 -0400

    Added the cdk to create a user in aws

---
+++ iam-user-cdk/stacks/iam-cdk-stack.js
@@ -0,0 +1,42 @@
+const cdk = require('@aws-cdk/core');
+const iam = require('@aws-cdk/aws-iam');
+
+class IamCdkStack extends cdk.Stack {
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    const userName = 'test-user'
+    const user = new iam.User(this, userName, {
+      userName: userName
+    });
+
+    user.addToPolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: ['cloudformation:*']
+    }));
+
+    user.addToPolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: ['*'],
+      conditions: {
+        'ForAnyValue:StringEquals': {
+          "aws:CalledVia": [
+            "cloudformation.amazonaws.com"
+          ]
+        }
+      }
+    }));
+
+    user.addToPolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['arn:aws:s3:::cdktoolkit-stagingbucket-*'],
+      actions: ['s3:*']
+    }));
+
+  }
+}
+
+module.exports = { IamCdkStack }
