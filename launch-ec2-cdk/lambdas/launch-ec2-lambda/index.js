0789b973a81f3f176a0ca81add55b5040d2021d5
Song Li <da_tou_li@yahoo.com>
2020-08-03 16:55:24 -0400

    Keep the code

---
+++ launch-ec2-cdk/lambdas/launch-ec2-lambda/index.js
@@ -0,0 +1,36 @@
+var AWS = require('aws-sdk');
+
+AWS.config.update({ 
+  region: 'us-east-1' 
+});
+
+exports.lambdaHandler = async (event, context) => {
+  
+  const image_id = 'ami-07300f2e42fc5a12b';
+  const instanceParams = {
+     ImageId: image_id, 
+     InstanceType: 't2.micro',
+     KeyName: 'my-key-pair',
+     MinCount: 1,
+     MaxCount: 1
+  };
+
+  let result
+  try {
+
+    const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+    result = await ec2.runInstances(instanceParams).promise();
+  } catch(ex) {
+    
+    console.log('Failed');
+    console.log(ex);
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    }, body: JSON.stringify(result)
+  };
+
+};
--- launch-ec2-cdk/stacks/launch-ec2-cdk-stack.js
+++ launch-ec2-cdk/stacks/launch-ec2-cdk-stack.js
@@ -6,25 +6,40 @@ class LaunchEc2CdkStack extends cdk.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    const create_role = (id, scope) => {
-      const role_name = `${id}-EC2-LAUNCH-ROLE`;
-      const role = new iam.Role(scope, role_name, {
-        roleName: role_name,
-        description: role_name,
-        assumedBy: new iam.CompositePrincipal(
-          new iam.ServicePrincipal('lambda.amazonaws.com'),
-        )
+    const create_lambda = (id, scope) => {
+
+      const create_role = (id, scope) => {
+        const role_name = `${id}-EC2-LAUNCH-ROLE`;
+        const role = new iam.Role(scope, role_name, {
+          roleName: role_name,
+          description: role_name,
+          assumedBy: new iam.CompositePrincipal(
+            new iam.ServicePrincipal('lambda.amazonaws.com'),
+          )
+        });
+  
+        role.addToPolicy(new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          resources: ['*'],
+          actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents', 'ec2:RunInstances', 'iam:PassRole']
+        }));
+      };
+
+      const role = create_role(id, scope);
+
+      const lambda_name = `${id}-LAUNCH_EC2_LAMBDA`;
+      const lambda_function = new lambda.Function(scope, lambda_name, {
+        runtime: lambda.Runtime.NODEJS_12_X,
+        functionName: lambda_name,
+        description: lambda_name,
+        timeout: cdk.Duration.seconds(5),
+        role: role,
+        code: lambda.Code.asset('./lambdas/launch-ec2-lambda'),
+        memorySize: 128,
+        handler: 'index.lambdaHandler'
       });
 
-      role.addToPolicy(new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        resources: ['*'],
-        actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents', 'ec2:RunInstances']
-      }));
-    };
-
-    const create_lambda = (id, scope) => {
-      const role = create_role(id, scope);
+      return lambda_function;
     };
 
     create_lambda(id, this);
