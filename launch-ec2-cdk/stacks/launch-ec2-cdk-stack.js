a8d65dab80b719b42d6913b7a23ca3679206aa7e
Song Li <da_tou_li@yahoo.com>
2020-08-03 15:26:45 -0400

    Adding the lambda

--- launch-ec2-cdk/stacks/launch-ec2-cdk-stack.js
+++ launch-ec2-cdk/stacks/launch-ec2-cdk-stack.js
@@ -1,16 +1,33 @@
 const cdk = require('@aws-cdk/core');
+const iam = require('@aws-cdk/aws-iam');
+const lambda = require('@aws-cdk/aws-lambda');
 
 class LaunchEc2CdkStack extends cdk.Stack {
-  /**
-   *
-   * @param {cdk.Construct} scope
-   * @param {string} id
-   * @param {cdk.StackProps=} props
-   */
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const create_role = (id, scope) => {
+      const role_name = `${id}-EC2-LAUNCH-ROLE`;
+      const role = new iam.Role(scope, role_name, {
+        roleName: role_name,
+        description: role_name,
+        assumedBy: new iam.CompositePrincipal(
+          new iam.ServicePrincipal('lambda.amazonaws.com'),
+        )
+      });
+
+      role.addToPolicy(new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        resources: ['*'],
+        actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents', 'ec2:RunInstances']
+      }));
+    };
+
+    const create_lambda = (id, scope) => {
+      const role = create_role(id, scope);
+    };
+
+    create_lambda(id, this);
   }
 }
 
