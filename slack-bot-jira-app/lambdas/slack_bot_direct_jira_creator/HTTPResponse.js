36d39a4b7054b96083a0c8400c8150f6d7bbb82b
Song Li <da_tou_li@yahoo.com>
2020-08-11 07:05:43 -0400

    Remove the folder

--- slack-bot-jira-app/lambdas/slack_bot_direct_jira_creator/HTTPResponse.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  OK: (v) => { v = v || 'OK'; return { statusCode: 200, body: v}; },
-  NotImplemented: (v) => { v = v || 'Not Implemented'; return { statusCode: 501, body: v}; },
-  BadRequest: (v) => { v = v || 'Bad Request'; return { statusCode: 400, body: v}; },
-  InternalServerError: (v) => { v = v || 'Internal Server Error'; return { statusCode: 500, body: v}; }
-};
