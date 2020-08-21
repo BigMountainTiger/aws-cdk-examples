d6b727cf4b681f9797419fd94522914f61df7099
Song Li <da_tou_li@yahoo.com>
2020-08-21 14:00:18 -0400

    Added the AWS.config template

--- fargate-task-cdk/docker/experiment-1/app.js
+++ fargate-task-cdk/docker/experiment-1/app.js
@@ -1,12 +1,46 @@
 const AWS = require('aws-sdk');
 
 const argv = process.argv;
-const delay = 10;
+const delay = 1;
+
+const bucket = 'logs.huge.head.li';
+
+// AWS.config.update(
+//   {
+//       accessKeyId: '',
+//       secretAccessKey: '',
+//       region: 'us-east-1'
+//   }
+// );
+
+const put_s3_object = async () => {
+  const s3 = new AWS.S3();
+  
+  const time = new Date().toISOString();
+  const TARGET_KEY = time.replace(/:/g, '-').replace(/\./g, '-');
+
+  const params = {
+    Bucket: bucket,
+    Key: TARGET_KEY,
+    Body: time
+  };
+
+  const result = await s3.putObject(params).promise(); 
+  return result;
+};
+
 
 (async () => {
 
   const r = await new Promise((rs, rj) => {
-    setTimeout(() => {
+    setTimeout(async () => {
+
+      try {
+        await put_s3_object();
+      } catch(ex) {
+        console.log(ex);
+      }
+
       rs(argv);
     }, delay * 1000);
   });
