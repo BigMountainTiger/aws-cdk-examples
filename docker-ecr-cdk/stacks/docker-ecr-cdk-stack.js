6578a286ec36d1864ef4fd054c8f28231c00711e
Song Li <da_tou_li@yahoo.com>
2020-08-24 16:16:05 -0400

    Made the first cdk deploy

--- docker-ecr-cdk/stacks/docker-ecr-cdk-stack.js
+++ docker-ecr-cdk/stacks/docker-ecr-cdk-stack.js
@@ -1,16 +1,18 @@
 const cdk = require('@aws-cdk/core');
+const ecr = require('@aws-cdk/aws-ecr');
 
 class DockerEcrCdkStack extends cdk.Stack {
-  /**
-   *
-   * @param {cdk.Construct} scope
-   * @param {string} id
-   * @param {cdk.StackProps=} props
-   */
+
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const REPOSITORY_ID = `${id}-REPOSITORY`;
+    const REPOSITORY_NAME = `experiment`;
+    new ecr.Repository(this, REPOSITORY_ID, {
+      repositoryName: REPOSITORY_NAME,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+      
+    })
   }
 }
 
