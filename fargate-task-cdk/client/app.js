752629653396527c73a51a7b8757cbbb1c873ad1
Song Li <da_tou_li@yahoo.com>
2020-08-25 19:07:12 -0400

    Added some comment

--- fargate-task-cdk/client/app.js
+++ fargate-task-cdk/client/app.js
@@ -4,13 +4,14 @@ AWS.config.update({region:'us-east-1'});
 (async () => {
   const ecs = new AWS.ECS();
 
+  // Subnet needs a NAT, not IGW, unless publicIp is assigned
   const params = {
     taskDefinition: 'FARGATETASKCDKSTACKFARGATETASKCDKSTACKFARGATE5599BAAB',
     cluster: 'FARGATE-TASK-CDK-STACK-CLUSTER',
     launchType: 'FARGATE',
     networkConfiguration: {
         awsvpcConfiguration: {
-            subnets: ['subnet-0787c104626f0ac16', 'subnet-0ca6298b7ebe790a8'],
+            subnets: ['subnet-0ca6298b7ebe790a8'],
             securityGroups: ['sg-00bfe081c5db3a3ed'],
             assignPublicIp: 'DISABLED'
         }
