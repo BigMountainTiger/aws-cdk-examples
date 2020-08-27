ee208dab57423751b58fc38186c3ed9da025808b
Song Li <da_tou_li@yahoo.com>
2020-08-27 16:21:33 -0400

    Added test lambda

--- vpc-example-cdk/stacks/vpc-example-cdk-stack.js
+++ vpc-example-cdk/stacks/vpc-example-cdk-stack.js
@@ -1,5 +1,7 @@
 const cdk = require('@aws-cdk/core');
 const ec2 = require("@aws-cdk/aws-ec2");
+const iam = require('@aws-cdk/aws-iam');
+const lambda = require('@aws-cdk/aws-lambda');
 
 class VpcExampleCdkStack extends cdk.Stack {
 
@@ -18,7 +20,40 @@ class VpcExampleCdkStack extends cdk.Stack {
       ],
     });
 
-    cdk.Tag.add(vpc, 'VPC-TAG', 'FARGATE-VPC');
+    //cdk.Tag.add(vpc, 'VPC-TAG', 'FARGATE-VPC');
+    cdk.Tags.of(vpc).add('VPC-TAG', 'FARGATE-VPC');
+
+    const ROLE_NAME = `${id}-LAMBDA_ROLE`;
+    const role = new iam.Role(this, ROLE_NAME, {
+      roleName: ROLE_NAME,
+      description: ROLE_NAME,
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    });
+
+    role.addToPolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: ['logs:CreateLogGroup',
+        'logs:CreateLogStream',
+        'logs:PutLogEvents',
+        'tag:GetResources',
+        'ec2:DescribeSecurityGroups',
+        'ec2:DescribeSubnets',
+        'ec2:DescribeVpcs'
+      ]
+    }));
+
+    const LAMBDA_NAME = `${id}-TAG-QUERY`;
+    new lambda.Function(this, LAMBDA_NAME, {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      functionName: LAMBDA_NAME,
+      description: LAMBDA_NAME,
+      timeout: cdk.Duration.seconds(15),
+      role: role,
+      code: lambda.Code.asset('./lambdas/tag-query'),
+      memorySize: 256,
+      handler: 'app.lambdaHandler'
+    });
     
   }
 }
