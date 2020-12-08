6a8eebe081e6344edfbcc5efd6b3183f42f4d925
Song Li <da_tou_li@yahoo.com>
2020-12-08 14:30:31 -0500

    Keep the code

--- docker-lambda-cdk/lambdas/IN/app.py
+++ docker-lambda-cdk/lambdas/IN/app.py
@@ -1,5 +1,15 @@
+import time
+import datetime
+import boto3
 
 def work():
+  bucket = 'logs.huge.head.li';
+  file_name = '/var/task/invoice-template.txt';
+  ts = datetime.datetime.fromtimestamp(time.time()).strftime(r'%Y-%m-%d-%H-%M-%S')
+  object_name = f'G-{ts}.docx'
+
+  s3 = boto3.resource('s3')
+  s3.meta.client.upload_file(file_name, bucket, object_name)
 
 
 def lambdaHandler(event, context):
