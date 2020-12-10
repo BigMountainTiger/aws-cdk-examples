f32cfec21c980146b366171d4e1ed018176b2eef
Song Li <da_tou_li@yahoo.com>
2020-12-10 13:32:22 -0500

    Revised the json format

--- docker-lambda-cdk/lambdas/IN/docx_merge/docx_merge.py
+++ docker-lambda-cdk/lambdas/IN/docx_merge/docx_merge.py
@@ -5,13 +5,16 @@ from docx import Document
 import util
 
 def merge(data):
-  replacement = r'Paul Kempa'
+
+  contact = data['contact']
+  invoice = data['invoice']
+
   wdoc = Document('/var/task/invoice-template')
 
-  util.docx_replace(wdoc, re.compile(r'{{customer-name}}') , replacement)
-  util.docx_replace(wdoc, re.compile(r'{{company-name}}') , r'Baltimore Steel Factory')
+  util.docx_replace(wdoc, re.compile(r'{{customer-name}}') , contact['name'])
+  util.docx_replace(wdoc, re.compile(r'{{company-name}}') , contact['company'])
   util.docx_replace(wdoc, re.compile(r'{{invoice-date}}') , datetime.date.today().strftime(r'%m/%d/%Y'))
-  util.docx_fill_data(wdoc, data)
+  util.docx_fill_data(wdoc, invoice)
 
   stream = io.BytesIO()
   wdoc.save(stream)
