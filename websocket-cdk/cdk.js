efeb3252b3b549c16006aba57a669524ae931fd5
Song Li <da_tou_li@yahoo.com>
2020-12-14 15:40:48 -0500

    How to create websocket by cdk?

--- websocket-cdk/cdk.js
+++ websocket-cdk/cdk.js
@@ -4,4 +4,5 @@ const cdk = require('@aws-cdk/core');
 const { WebsocketCdkStack } = require('./stacks/websocket-cdk-stack');
 
 const app = new cdk.App();
-new WebsocketCdkStack(app, 'WebsocketCdkStack');
+const NAME = 'WEBSOCKET-CDK-STACK';
+new WebsocketCdkStack(app, NAME);
--- websocket-cdk/stacks/websocket-cdk-stack.js
+++ websocket-cdk/stacks/websocket-cdk-stack.js
@@ -1,10 +1,22 @@
 const cdk = require('@aws-cdk/core');
+const apigateway = require('@aws-cdk/aws-apigateway');
 
 class WebsocketCdkStack extends cdk.Stack {
 
   constructor(scope, id, props) {
     super(scope, id, props);
 
+    const API_NAME = `${id}-WEBSOCKET-API`;
+    const api = new apigateway.RestApi(this, API_NAME, {
+      restApiName: API_NAME,
+      description: API_NAME,
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS
+      },
+      endpointTypes: [apigateway.EndpointType.REGIONAL]
+    });
+    
     
   }
 }
