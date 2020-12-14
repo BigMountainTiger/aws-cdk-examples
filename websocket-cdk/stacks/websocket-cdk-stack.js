71f473186dc3c4f3c72955d91070c7255909c1c0
Song Li <da_tou_li@yahoo.com>
2020-12-14 17:24:58 -0500

    Keep the code

--- websocket-cdk/stacks/websocket-cdk-stack.js
+++ websocket-cdk/stacks/websocket-cdk-stack.js
@@ -1,9 +1,9 @@
 // Websocket is still not fully supported by aws-cdk
-// https://stackoverflow.com/questions/63416026/aws-cdk-construct-in-c-sharp-for-api-gateway-websockets
-// https://github.com/aws-samples/aws-cdk-examples/pull/325/files
 
 const cdk = require('@aws-cdk/core');
-const apigateway = require('@aws-cdk/aws-apigateway');
+const apiv2 = require('@aws-cdk/aws-apigatewayv2');
+const dynamodb = require('@aws-cdk/aws-dynamodb');
+const iam = require("@aws-cdk/aws-iam");
 
 class WebsocketCdkStack extends cdk.Stack {
 
@@ -11,16 +11,28 @@ class WebsocketCdkStack extends cdk.Stack {
     super(scope, id, props);
 
     const API_NAME = `${id}-WEBSOCKET-API`;
-    const api = new apigateway.RestApi(this, API_NAME, {
-      restApiName: API_NAME,
+    const api = new apiv2.CfnApi(this, API_NAME, {
+      name: API_NAME,
       description: API_NAME,
-      defaultCorsPreflightOptions: {
-        allowOrigins: apigateway.Cors.ALL_ORIGINS,
-        allowMethods: apigateway.Cors.ALL_METHODS
-      },
-      endpointTypes: [apigateway.EndpointType.REGIONAL]
+      protocolType: "WEBSOCKET",
+      routeSelectionExpression: "$request.body.action",
     });
     
+    const DYNAMO_NAME = `${id}-DYNAMO-TABLE`;
+    const table = new dynamodb.Table(this, DYNAMO_NAME, {
+      tableName: DYNAMO_NAME,
+      partitionKey: { name: "connectionId", type: dynamodb.AttributeType.STRING },
+      readCapacity: 5,
+      writeCapacity: 5,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    const LAMBDA_ROLE_NAME = `${id}-LAMBDA-ROLE`;
+    const lambda_role = new iam.Role(this, LAMBDA_ROLE_NAME, {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com")
+    });
+    lambda_role.addToPolicy(new iam.PolicyStatement({ actions: [ 'dynamodb:*' ], resources: [table.tableArn] }));
+    lambda_role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'));
     
   }
 }
