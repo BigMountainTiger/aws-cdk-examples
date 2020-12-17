684cb40d9443e8dc6d6e2a338fc1bd785835af85
Song Li <da_tou_li@yahoo.com>
2020-12-17 11:57:59 -0500

    Added the auto-refresh to the index.html

--- websocket-cdk/client/index.js
+++ websocket-cdk/client/index.js
@@ -1,3 +1,5 @@
+// https://www.npmjs.com/package/browser-refresh
+
 const express = require('express'),
   http = require('http'),
   path = require('path'),
@@ -7,18 +9,22 @@ const app = express();
     
 app.set('port', 3000);
     
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
   res.header('Expires', '-1');
   res.header('Pragma', 'no-cache');
   next();
 });
-    
+
 app.use(express.static(path.join(__dirname, 'static')));
-    
 app.use(errorhandler());
+
+app.get('/refresh-script', (req, res) => {
+  const refresh_script = process.env.BROWSER_REFRESH_URL || ''
+  res.send(refresh_script);
+});
     
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), () => {
   console.log(`Express server listening on port ${app.get('port')}`);
 
   if (process.send) {
