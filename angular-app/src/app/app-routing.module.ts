85f741401269f60705ba48bd1b3a428f94ebd1a7
Song Li <da_tou_li@yahoo.com>
2021-02-15 17:55:06 -0500

    Moved the example into the routing component

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CognitoLoginExampleComponent } from './components/cognito-login-example/cognito-login-example.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
 
-  { path: 'cognito-login-example', component: CognitoLoginExampleComponent }
+  { path: '', redirectTo: '/cognito-login-example', pathMatch: 'full' },
+  { path: 'cognito-login-example', component: CognitoLoginExampleComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  declarations: [CognitoLoginExampleComponent]
+  declarations: [CognitoLoginExampleComponent, PageNotFoundComponent]
 })
 export class AppRoutingModule { }
--- angular-app/src/app/components/cognito-login-example/cognito-login-example.component.ts
+++ angular-app/src/app/components/cognito-login-example/cognito-login-example.component.ts
@@ -1,4 +1,7 @@
+// https://docs.amazonaws.cn/en_us/cognito/latest/developerguide/token-endpoint.html
+
 import { Component, OnInit } from '@angular/core';
+import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js';
 
 @Component({
   selector: 'app-cognito-login-example',
@@ -10,6 +13,55 @@ export class CognitoLoginExampleComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  async login(user, pwd) {
+    const USER_POOL_ID = 'us-east-1_n37H054sQ';
+    const USER_POOL_CLIENT_ID = '1pd3ri7i655lk7bo39e3bppn23';
+    let USER = user;
+    let PASSWORD = pwd;
+
+    const poolData = {    
+      UserPoolId : USER_POOL_ID, 
+      ClientId : USER_POOL_CLIENT_ID
+    }; 
+
+    const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+    const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({ Username: USER, Password: PASSWORD, });
+    const cognitoUser = new AmazonCognitoIdentity.CognitoUser({ Username: USER, Pool: userPool });
+
+    const p = new Promise((rs, rj) => {
+      const callback = {
+        onSuccess: (result) => {
+          const data = {
+            access_token: result.getAccessToken().getJwtToken(),
+            id_token: result.getIdToken().getJwtToken(),
+            refresh_token: result.getRefreshToken().getToken()
+          }
+
+          rs(data);
+        },
+        onFailure: (e) => { 
+          rj(e);
+        },
+        newPasswordRequired: (userAttributes) => {
+          delete userAttributes.email_verified;
+          cognitoUser.completeNewPasswordChallenge(PASSWORD, userAttributes, callback);
+        }
+      }
+
+      cognitoUser.authenticateUser(authenticationDetails, callback);
+    });
+
+    return await p;
+  }
+
+  async onTest() {
+    const USER = 'song';
+    const PASSWORD = 'Password123';
+
+    const token = await this.login(USER, PASSWORD);
+    console.log(token);
   }
 
 }
