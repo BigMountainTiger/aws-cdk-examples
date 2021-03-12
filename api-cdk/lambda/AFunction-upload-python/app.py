8cff6b4d227078780267ccc0ccf80f994f1e4abc
Song Li <da_tou_li@yahoo.com>
2021-03-12 14:57:57 -0500

    Tested python multi-part support

--- api-cdk/lambda/AFunction-upload-python/app.py
+++ api-cdk/lambda/AFunction-upload-python/app.py
@@ -1,26 +1,34 @@
 # https://stackoverflow.com/questions/50925083/parse-multipart-request-string-in-python
 
+import boto3
 import base64
 from requests_toolbelt.multipart import decoder
 import json
+import time
+
+bucket = 'api-cdk.huge.head.li'
 
 def lambdaHandler(event, context):
-    
-    # content_type = event['headers']['Content-Type'];
-    # body = base64.b64decode(event['body']).decode('ISO-8859-1')
-    
-    # form_data = decoder.MultipartDecoder(body.encode('utf-8'), content_type).parts
-    
-    # print(form_data[0])
-    # print(form_data[1])
-    
-    
-    # multipart_string = b"--ce560532019a77d83195f9e9873e16a1\r\nContent-Disposition: form-data; name=\"author\"\r\n\r\nJohn Smith\r\n--ce560532019a77d83195f9e9873e16a1\r\nContent-Disposition: form-data; name=\"file\"; filename=\"example2.txt\"\r\nContent-Type: text/plain\r\nExpires: 0\r\n\r\nHello World\r\n--ce560532019a77d83195f9e9873e16a1--\r\n"
-    # content_type = "multipart/form-data; boundary=ce560532019a77d83195f9e9873e16a1"
-    # result = decoder.MultipartDecoder(multipart_string, content_type).parts
 
-    # print(result)
+    content_type = event['headers']['Content-Type']
+    body = base64.b64decode(event['body'])
+    
+    form_data = decoder.MultipartDecoder(body, content_type).parts
+    part = form_data[0]
+
+    # print(part.headers)
+    # print(part.content)
+    
+    key = f'F-{int(time.time())}.jpg'
+    client = boto3.client('s3')
+    def get_presigned_url():
+        client.put_object(Body = part.content, Bucket = bucket, Key = key)
+        # ExpiresIn - time in second
+        return client.generate_presigned_url('get_object', Params = { 'Bucket': bucket, 'Key': key }, ExpiresIn = 100)
+
+    presigned_url = get_presigned_url()
+
     return {
         'statusCode': 200,
-        'body': json.dumps(event)
+        'body': presigned_url
     }
