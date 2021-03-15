b6fcd68272f075e5b628abe15b8cf3e0545e663c
Song Li <da_tou_li@yahoo.com>
2021-03-15 15:07:34 -0400

    Tested the PUT/POST presigned URL

--- api-cdk/lambda/AFunction-upload-presigned-python/app.py
+++ api-cdk/lambda/AFunction-upload-presigned-python/app.py
@@ -1,4 +1,7 @@
+# https://stackoverflow.com/questions/65198959/aws-s3-generate-presigned-url-vs-generate-presigned-post-for-uploading-files
+
 import boto3, json, time
+from botocore.config import Config
 
 bucket = 'api-cdk.huge.head.li'
 
@@ -7,14 +10,27 @@ def lambdaHandler(event, context):
     
     key = f'F-{int(time.time())}.jpg'
 
-    client = boto3.client('s3')
-    # def get_presigned_url():
-    #     return client.generate_presigned_url('get_object', Params = { 'Bucket': bucket, 'Key': key }, ExpiresIn = 60 * 60 * 24)
+    client = boto3.client('s3', config=Config(signature_version='s3v4'))
+    def get_presigned_url():
+        expire = 60 * 60 * 24
 
-    # presigned_url = get_presigned_url()
+        # PUT - use binary to send data from POSTMAN
+        # return client.generate_presigned_url('put_object', Params = { 'Bucket': bucket, 'Key': key }, ExpiresIn = expire)
+
+        # POST - testing
+        return client.generate_presigned_post(Bucket = bucket, Key = key, ExpiresIn = expire)
+
+    presigned_url = get_presigned_url()
+
+    print(presigned_url)
+
+    result = {
+        'key': key,
+        'presigned_url': presigned_url
+    }
 
     return {
         'statusCode': 200,
         'headers': { 'Access-Control-Allow-Origin': '*', 'content-type': 'application/json' },
-        'body': json.dumps(key)
+        'body': json.dumps(result)
     }
