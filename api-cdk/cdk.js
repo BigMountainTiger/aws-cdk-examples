302548b9a66d43997407141aeaa83b6d26c77d74
Song Li <da_tou_li@yahoo.com>
2021-03-24 13:25:41 -0400

    Renamed the directories

--- api-cdk/cdk.js
+++ api-cdk/cdk.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const cdk = require('@aws-cdk/core');
-const { ApiCdkStack } = require('./lib/api-cdk-stack');
+const { ApiCdkStack } = require('./stack/api-cdk-stack');
 
 const NAME = 'API-CDK-STACK';
 const app = new cdk.App();
