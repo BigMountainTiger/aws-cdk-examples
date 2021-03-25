846ee571a54b76090a4ea4cb4e6358e8f43959f5
Song Li <da_tou_li@yahoo.com>
2021-03-25 11:07:07 -0400

    Added the template cdk

---
+++ code-artifact-cdk/stacks/code-artifact-cdk-stack.js
@@ -0,0 +1,12 @@
+const cdk = require('@aws-cdk/core');
+
+class CodeArtifactCdkStack extends cdk.Stack {
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    
+  }
+}
+
+module.exports = { CodeArtifactCdkStack }
