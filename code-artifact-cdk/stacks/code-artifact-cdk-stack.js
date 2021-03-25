bb4ef218f9da20a54b88dddd791b18efc5d2ed80
Song Li <da_tou_li@yahoo.com>
2021-03-25 11:54:20 -0400

    Added the code-artifact

--- code-artifact-cdk/stacks/code-artifact-cdk-stack.js
+++ code-artifact-cdk/stacks/code-artifact-cdk-stack.js
@@ -1,11 +1,23 @@
 const cdk = require('@aws-cdk/core');
+const codeartifact = require('@aws-cdk/aws-codeartifact');
 
 class CodeArtifactCdkStack extends cdk.Stack {
 
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    
+    const NAME = 'test-nuget-repo';
+    const DOMAIN_NAME = 'test-domain';
+
+    new codeartifact.CfnDomain(this, `${id}-nuget-domain`, {
+      domainName: DOMAIN_NAME
+    });
+
+    new codeartifact.CfnRepository(this, `${id}-nuget-repository`, {
+      domainName: DOMAIN_NAME,
+      repositoryName: NAME,
+      description: NAME
+    })
   }
 }
 
