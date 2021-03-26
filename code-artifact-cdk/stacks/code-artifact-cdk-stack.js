fcd52976b013097b2f97fec7985ebc892bbe6860
Song Li <da_tou_li@yahoo.com>
2021-03-26 14:33:07 -0400

    Tested using env to set aws permissions

--- code-artifact-cdk/stacks/code-artifact-cdk-stack.js
+++ code-artifact-cdk/stacks/code-artifact-cdk-stack.js
@@ -1,4 +1,5 @@
 const cdk = require('@aws-cdk/core');
+const iam = require('@aws-cdk/aws-iam');
 const codeartifact = require('@aws-cdk/aws-codeartifact');
 
 class CodeArtifactCdkStack extends cdk.Stack {
@@ -9,9 +10,32 @@ class CodeArtifactCdkStack extends cdk.Stack {
     const DOMAIN_NAME = 'testdomain';
     const REPO_NAME = 'testnugetrepo';
 
-    new codeartifact.CfnDomain(this, `${id}-nuget-domain`, {
+    const domain = new codeartifact.CfnDomain(this, `${id}-nuget-domain`, {
       domainName: DOMAIN_NAME
     });
+
+    const document = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          resources: ['*'],
+          actions: ['codeartifact:*'],
+          principals: [
+            new iam.ArnPrincipal('arn:aws:iam::537548412289:root')
+          ]
+        }),
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          resources: ['*'],
+          actions: ['codeartifact:*'],
+          principals: [
+            new iam.ArnPrincipal('arn:aws:iam::123129172005:root')
+          ]
+        })
+      ]
+    });
+
+    domain.permissionsPolicyDocument = document;
 
     new codeartifact.CfnRepository(this, `${id}-nuget-repository`, {
       domainName: DOMAIN_NAME,
