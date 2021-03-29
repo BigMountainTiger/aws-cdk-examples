e1c1d1f5930725f3469dedb1cc3990af3cfc458f
Song Li <da_tou_li@yahoo.com>
2021-03-29 14:16:32 -0400

    Kee the files

---
+++ dotnet-lambda-layer-cdk/nuget/ExampleNuget/ExampleNuget.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ExampleNuget
+{
+    public class ExampleNuget
+    {
+        public static string GetAString() {
+            return "This is the string from the classlib";
+        }
+    }
+}
