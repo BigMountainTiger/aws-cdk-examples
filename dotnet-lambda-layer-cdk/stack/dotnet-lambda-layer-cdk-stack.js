ea0ebb604e2a263cda4af9f7b91430895ee21650
Song Li <da_tou_li@yahoo.com>
2021-03-29 12:00:29 -0400

    Test deployed the stack

--- dotnet-lambda-layer-cdk/stack/dotnet-lambda-layer-cdk-stack.js
+++ dotnet-lambda-layer-cdk/stack/dotnet-lambda-layer-cdk-stack.js
@@ -1,11 +1,31 @@
 const cdk = require('@aws-cdk/core');
+const iam = require('@aws-cdk/aws-iam');
+const lambda = require('@aws-cdk/aws-lambda');
 
 class DotnetLambdaLayerCdkStack extends cdk.Stack {
 
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    
+    const ROLE_NAME = `${id}-Lambda_Role`;
+
+    const add_lambda_role = () => {
+      const role = new iam.Role(this, ROLE_NAME, {
+        roleName: ROLE_NAME,
+        assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      });
+
+      role.addToPolicy(new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        resources: ['*'],
+        actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents']
+      }));
+
+      return role;
+    };
+
+    const role = add_lambda_role();
+
   }
 }
 
