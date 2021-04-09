5cfdc7661ae68c1aba85ce6161e42b9f48ad9c10
Song Li <da_tou_li@yahoo.com>
2021-04-09 15:51:51 -0400

    Added the animation

--- angular-app/src/app/cropper-example/just-popup/just-popup.component.ts
+++ angular-app/src/app/cropper-example/just-popup/just-popup.component.ts
@@ -7,13 +7,32 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./just-popup.component.css']
 })
 export class JustPopupComponent implements OnInit {
+  // INIT, LOADING, FAIL, DONE
+  public status: string
 
   constructor(private _modalService: NgbModal) { }
 
   ngOnInit(): void {
+    this.status = 'INIT';
   }
 
   public onClose(): void {
     this._modalService.dismissAll();
+  }
+
+  public handleFileInput(files): void {
+    const file = files[0];
+    const types = ['image/jpeg', 'image/png'];
+
+
+    if ((! types.includes(file.type)) || (file.size > (4 * 1024 * 1024))) {
+      this.status = 'FAIL';
+      return;
+    }
+
+    this.status = 'LOADING';
+    setTimeout(() => {
+      this.status = 'DONE';
+    }, 1000);
   }
 }
