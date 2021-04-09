33a63963bf25ed111e9c35780dcf8dc228138105
Song Li <da_tou_li@yahoo.com>
2021-04-09 16:30:15 -0400

    Keep the code

--- angular-app/src/app/cropper-example/just-popup/just-popup.component.ts
+++ angular-app/src/app/cropper-example/just-popup/just-popup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -6,11 +6,52 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './just-popup.component.html',
   styleUrls: ['./just-popup.component.css']
 })
-export class JustPopupComponent implements OnInit {
+export class JustPopupComponent implements OnInit, AfterViewInit {
   // INIT, LOADING, FAIL, DONE
   public status: string
 
   constructor(private _modalService: NgbModal) { }
+
+  ngAfterViewInit(): void {
+    const dropArea = document.getElementById("profile-picture-file-drop-container");
+
+    const preventDefaults = (e) => {
+      e.preventDefault()
+      e.stopPropagation()
+    };
+
+    const highlight = (highlight) => {
+      if (highlight == 'on')
+        dropArea.classList.add('on');
+      else
+        dropArea.classList.remove('on');
+    };
+
+    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+      dropArea.addEventListener(eventName, preventDefaults, false);
+      document.body.addEventListener(eventName, preventDefaults, false);
+    });
+
+    ['dragenter', 'dragover'].forEach(eventName => {
+      dropArea.addEventListener(eventName, () => {
+        highlight('on');
+      }, false)
+    });
+
+    ['dragleave', 'drop'].forEach(eventName => {
+      dropArea.addEventListener(eventName, () => {
+        highlight('off');
+      }, false)
+    });
+
+    dropArea.addEventListener('drop', (e) => {
+      var dt = e.dataTransfer
+      var files = dt.files;
+
+      this.handleFileInput(files);
+
+    }, false);
+  }
 
   ngOnInit(): void {
     this.status = 'INIT';
