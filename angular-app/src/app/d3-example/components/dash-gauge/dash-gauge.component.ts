5506ea85fec3950adc76e785827bea29d2391dd0
Song Li <da_tou_li@yahoo.com>
2021-05-10 11:57:47 -0400

    Keep the code

--- angular-app/src/app/d3-example/components/dash-gauge/dash-gauge.component.ts
+++ angular-app/src/app/d3-example/components/dash-gauge/dash-gauge.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dash-gauge',
   templateUrl: './dash-gauge.component.html',
-  styleUrls: ['./dash-gauge.component.css']
+  styleUrls: ['./dash-gauge.component.scss']
 })
 export class DashGaugeComponent implements OnInit {
 
