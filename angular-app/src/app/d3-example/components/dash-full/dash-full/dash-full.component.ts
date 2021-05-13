59894c05cddc32f6b1903b55aa2dbc9a570430e9
Song Li <da_tou_li@yahoo.com>
2021-05-13 19:03:18 -0400

    Keep the code

--- angular-app/src/app/d3-example/components/dash-full/dash-full/dash-full.component.ts
+++ angular-app/src/app/d3-example/components/dash-full/dash-full/dash-full.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dash-full',
   templateUrl: './dash-full.component.html',
-  styleUrls: ['./dash-full.component.css']
+  styleUrls: ['./dash-full.component.scss']
 })
 export class DashFullComponent implements OnInit {
 
