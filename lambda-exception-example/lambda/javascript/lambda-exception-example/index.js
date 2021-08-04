2dbaf2cdbb4304c3628d7dd317834bfc2e1a49d1
Song Li <da_tou_li@yahoo.com>
2021-08-04 15:35:37 -0400

    Keep the code

--- lambda-exception-example/lambda/javascript/lambda-exception-example/index.js
+++ lambda-exception-example/lambda/javascript/lambda-exception-example/index.js
@@ -4,23 +4,40 @@ exports.handler = async (event, context) => {
 
     setTimeout(() => {
       try {
-        throw 'This is stupid';
 
+        throw {
+          messages: [
+            '1. Exceptions thrown in callback functions can only be caught inside the callback function',
+            '2. Exceptions thrown in callback functions are not caught by the code awaiting the promise',
+            '3. Uncaught exceptions do not kill the lambda because it is running inside a container',
+            '4. A promise rejection can be caught by the code awaiting the promise',
+            '5. The exception will put an entry in the cloudwatch log',
+            '6. A new cloudwatch stream is started per lambda container in the log group'
+          ]
+        };
+
+        // This resolve will never happen
+        // because throw happens before it
         rs({
-          message: 'Each cloudwatch log stream is started by a new lambda container'
+          message: 'Promise is resolved'
         });
       }
       catch(e) {
+
+        // Exception thrown in the callback functions
+        // can only be caught inside the callback function
         rj(e);
       }
 
     }, 0.1 * 1000);
 
-    throw 'This is stupid';
   });
 
   const result = {};
-  try { result.message = await p; } catch(e) { result.error = e; }
+  try { result.message = await p; } catch(e) {
+    // Promise rejection can be caught as an exception
+    result['this-is-an-artificial-error'] = e;
+  }
 
   return {
     statusCode: 200,
