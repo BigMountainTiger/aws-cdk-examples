a78687d9f4130e7908edc902ebdcccd9da1395e8
Song Li <da_tou_li@yahoo.com>
2022-05-21 14:41:27 -0400

    Added fargate-alb-example

---
+++ fargate/fargate-alb-example/stacks/fargate-alb-example-stack.js
@@ -0,0 +1,12 @@
+const { Stack, Duration } = require('aws-cdk-lib');
+// const sqs = require('aws-cdk-lib/aws-sqs');
+
+class FargateAlbExampleStack extends Stack {
+  
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+  }
+}
+
+module.exports = { FargateAlbExampleStack }
